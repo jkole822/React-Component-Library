7e3ce1541e82c1357b980d837634dda932206b65
kolegasior <jkole822@gmail.com>
2025-04-13 17:34:02 -0400

    feature: ComboBox - Create

---
+++ src/lib/ComboBox/index.tsx
@@ -0,0 +1,135 @@
+// Packages
+import {
+  Combobox as ArkComboBox,
+  createListCollection,
+} from "@ark-ui/react/combobox";
+import { Field } from "@ark-ui/react/field";
+import { Portal } from "@ark-ui/react/portal";
+import { useMemo, useState } from "react";
+
+// Styles
+import {
+  ClearTriggerIconStyles,
+  ClearTriggerStyles,
+  ContentStyles,
+  ControlStyles,
+  DescriptionStyles,
+  ErrorMessageStyles,
+  InputStyles,
+  ItemDescriptionStyles,
+  ItemIndicatorStyles,
+  ItemLabelStyles,
+  ItemStyles,
+  ItemTextStyles,
+  LabelStyles,
+  TriggerStyles,
+} from "./styles";
+
+// Types
+import { ComboBoxValidationStateEnum, type Props } from "./types";
+
+function ComboBoxRoot({ items: initialItems, name, value, ...rest }: Props) {
+  const [items, setItems] = useState(initialItems);
+
+  const collection = useMemo(
+    () =>
+      createListCollection({
+        items: items.map((item) => ({ ...item, value: item.id })),
+      }),
+    [items]
+  );
+
+  const handleInputChange = (details: ArkComboBox.InputValueChangeDetails) => {
+    setItems(
+      initialItems.filter((item) =>
+        item.label.toLowerCase().includes(details.inputValue.toLowerCase())
+      )
+    );
+  };
+
+  return (
+    <ArkComboBox.Root
+      {...rest}
+      collection={collection}
+      name={name}
+      onInputValueChange={handleInputChange}
+      value={value}
+    >
+      <ArkComboBox.Label className={LabelStyles}>{name}</ArkComboBox.Label>
+      <ArkComboBox.Control className={ControlStyles}>
+        <ArkComboBox.Input className={InputStyles} />
+        <ArkComboBox.Trigger aria-label="open" className={TriggerStyles}>
+          <i aria-hidden="true" className="fa-solid fa-sort"></i>
+        </ArkComboBox.Trigger>
+        <ArkComboBox.ClearTrigger
+          aria-label="clear"
+          className={ClearTriggerStyles}
+        >
+          <i aria-hidden="true" className={ClearTriggerIconStyles}></i>
+        </ArkComboBox.ClearTrigger>
+      </ArkComboBox.Control>
+      <Portal>
+        <ArkComboBox.Positioner>
+          <ArkComboBox.Content className={ContentStyles}>
+            {collection.items.map((item) => (
+              <ArkComboBox.Item
+                className={ItemStyles}
+                key={item.label}
+                item={item}
+              >
+                <ArkComboBox.ItemText className={ItemTextStyles}>
+                  <span className={ItemLabelStyles}>{item.label}</span>
+                  <span className={ItemDescriptionStyles}>
+                    {item.description}
+                  </span>
+                </ArkComboBox.ItemText>
+                <ArkComboBox.ItemIndicator className={ItemIndicatorStyles}>
+                  ✓
+                </ArkComboBox.ItemIndicator>
+              </ArkComboBox.Item>
+            ))}
+          </ArkComboBox.Content>
+        </ArkComboBox.Positioner>
+      </Portal>
+    </ArkComboBox.Root>
+  );
+}
+
+function ComboBoxWithField({
+  description,
+  disabled,
+  errorMessage,
+  readOnly,
+  required,
+  validationState,
+  ...rest
+}: Props) {
+  return (
+    <Field.Root
+      disabled={disabled}
+      invalid={validationState === ComboBoxValidationStateEnum.Invalid}
+      readOnly={readOnly}
+      required={required}
+    >
+      <ComboBoxRoot {...rest} />
+      {!!description && (
+        <Field.HelperText className={DescriptionStyles}>
+          {description}
+        </Field.HelperText>
+      )}
+      {!!errorMessage && (
+        <Field.ErrorText className={ErrorMessageStyles}>
+          {errorMessage}
+        </Field.ErrorText>
+      )}
+    </Field.Root>
+  );
+}
+
+export default function ComboBox({ withField, ...rest }: Props) {
+  if (withField) {
+    return <ComboBoxWithField {...rest} />;
+  }
+
+  return <ComboBoxRoot {...rest} />;
+}
---
+++ src/lib/ComboBox/story.tsx
@@ -0,0 +1,32 @@
+// Packages
+import { useState } from "react";
+
+// Components
+import ComboBox from ".";
+
+// Styles
+import { PararaphStyles, SubHeadingStyles } from "../../styles";
+
+// Types
+import type { ComboBoxItem, Props } from "./types";
+
+export default function ComboBoxStory(props: Props) {
+  const [value, setValue] = useState<string[]>([]);
+
+  const handleChange = (update: { items: ComboBoxItem[]; value: string[] }) => {
+    setValue(update.value);
+  };
+
+  return (
+    <>
+      <ComboBox {...props} onValueChange={handleChange} value={value} />
+      <p className={SubHeadingStyles}>Binding Check</p>
+      <p className={PararaphStyles}>Selected Item IDs:</p>
+      <div className="flex flex-col gap-0.5 mt-1 text-sm">
+        {value.map((id) => (
+          <span key={id}>{id}</span>
+        ))}
+      </div>
+    </>
+  );
+}
