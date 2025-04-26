13c9b2c08dc94c5021e1d091f038977308bbbc92
kolegasior <jkole822@gmail.com>
2025-04-26 08:37:16 -0500

    feature: Toast - Create

---
+++ src/lib/Toast/index.tsx
@@ -0,0 +1,63 @@
+// Packages
+import { createToaster, Toast as ArkToast, Toaster } from "@ark-ui/react/toast";
+import { useEffect, useRef } from "react";
+
+// Styles
+import {
+  CloseButtonStyles,
+  DescriptionStyles,
+  RootStyles,
+  TitleStyles,
+} from "./styles";
+
+// Types
+import { ToastTypeEnum } from "./types";
+import type { Props } from "./types";
+
+const toaster = createToaster({
+  placement: "bottom-end",
+  overlap: true,
+  gap: 24,
+});
+
+export default function Toast({ toast }: Props) {
+  const id = useRef("");
+
+  const createToast = () => {
+    if (!toast) return;
+
+    id.current = toaster.create(toast);
+  };
+
+  const updateToast = () => {
+    if (!id.current) return;
+
+    if (!!toast) toaster.update(id.current, toast);
+  };
+
+  useEffect(() => {
+    if (toast?.type === ToastTypeEnum.Create) {
+      createToast();
+    } else {
+      updateToast();
+    }
+  }, [toast]);
+
+  return (
+    <Toaster toaster={toaster}>
+      {(toast) => (
+        <ArkToast.Root key={toast.id} className={RootStyles}>
+          <ArkToast.Title className={TitleStyles}>{toast.title}</ArkToast.Title>
+          {!!toast.description && (
+            <ArkToast.Description className={DescriptionStyles}>
+              {toast.description}
+            </ArkToast.Description>
+          )}
+          <ArkToast.CloseTrigger className={CloseButtonStyles}>
+            <i aria-hidden="true" className="fa-solid fa-xmark"></i>
+          </ArkToast.CloseTrigger>
+        </ArkToast.Root>
+      )}
+    </Toaster>
+  );
+}
---
+++ src/lib/Toast/story.tsx
@@ -0,0 +1,44 @@
+// Packages
+import { faker } from "@faker-js/faker";
+import { useState } from "react";
+
+// Components
+import Button from "../Button";
+import Toast from ".";
+
+// Types
+import { ButtonVariantsEnum } from "../Button/types";
+import { ToastTypeEnum } from "./types";
+import type { IToast } from "./types";
+
+export default function ToastStory() {
+  const [toast, setToast] = useState<IToast | undefined>();
+
+  const createToast = () => {
+    setToast({
+      title: faker.lorem.words(2),
+      description: faker.lorem.sentence(),
+      type: ToastTypeEnum.Create,
+    });
+  };
+
+  const updateToast = () => {
+    setToast({
+      title: faker.lorem.words(2),
+      description: faker.lorem.sentence(),
+      type: ToastTypeEnum.Update,
+    });
+  };
+
+  return (
+    <div>
+      <div className="flex gap-4">
+        <Button onClick={createToast}>Create Toast</Button>
+        <Button onClick={updateToast} variant={ButtonVariantsEnum.outline}>
+          Update Toast
+        </Button>
+      </div>
+      <Toast toast={toast}></Toast>
+    </div>
+  );
+}
