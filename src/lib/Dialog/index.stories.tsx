f0f788dcc79142d7a59d96f81ac48ae8a0d5cbd1
kolegasior <jkole822@gmail.com>
2025-05-04 09:43:02 -0400

    feature: Dialog - Enhance Storybook docs with prop descriptions, enable Button disabled styles

--- src/lib/Dialog/index.stories.tsx
+++ src/lib/Dialog/index.stories.tsx
@@ -11,26 +11,68 @@ const meta = {
   title: "Dialog",
   component: Dialog,
   tags: ["autodocs"],
-  argTypes: {},
+  args: {
+    description: faker.lorem.paragraph(),
+    defaultOpen: false,
+    modal: true,
+    title: faker.lorem.words(2),
+  },
+  argTypes: {
+    children: {
+      description:
+        "The React element or content to be rendered inside the component.",
+      table: {
+        type: { summary: "ReactNode" },
+      },
+    },
+    defaultOpen: {
+      control: "boolean",
+      description:
+        "The open state of the dialog when it is initially rendered.",
+    },
+    description: {
+      control: "text",
+      description:
+        "The description to be rendered within, and describes, the dialog.",
+    },
+    modal: {
+      control: "boolean",
+      description:
+        "When true, interaction with outside elements will be disabled and only dialog content will be visible to screen readers.",
+    },
+    onOpenChange: {
+      action: "onOpenChange",
+      description:
+        "Event handler called when the open state of the dialog changes.",
+      table: {
+        category: "Events",
+      },
+    },
+    open: {
+      control: "boolean",
+      description: "The controlled open state of the dialog.",
+    },
+    title: {
+      control: "text",
+      description: "The title to be rendered within, and labels, the dialog.",
+    },
+    trigger: {
+      description:
+        "The React element or content to be rendered as the component trigger.",
+      table: {
+        type: { summary: "ReactNode" },
+      },
+    },
+  },
 } satisfies Meta<typeof Dialog>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const args = {
-  description: faker.lorem.paragraph(),
-  defaultOpen: false,
-  modal: true,
-  title: faker.lorem.words(2),
-};
-
-export const Basic: Story = {
-  args,
-};
+export const Basic: Story = {};
 
 export const DefaultOpen: Story = {
   args: {
-    ...args,
     defaultOpen: true,
   },
 };
