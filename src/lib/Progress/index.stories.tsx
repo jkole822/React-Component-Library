533eb4bcf2fff8625a190bc11c778f2b85c994e2
kolegasior <jkole822@gmail.com>
2025-05-04 13:45:13 -0400

    feature + bugfix: Popover - Enhance Storybook docs with prop descriptions, indicator styles so that progress expands linearly

--- src/lib/Progress/index.stories.tsx
+++ src/lib/Progress/index.stories.tsx
@@ -11,18 +11,45 @@ const meta = {
   title: "Progress",
   component: Progress,
   tags: ["autodocs"],
+  args: {
+    asChild: false,
+    label: faker.lorem.word(),
+    max: 100,
+    value: 0,
+  },
   argTypes: {
-    className: { control: "text" },
+    asChild: {
+      control: "boolean",
+      description: "Renders the component as a child element.",
+    },
+    className: {
+      control: "text",
+      description: "Additional class name(s) applied to the root container.",
+    },
+    getValueLabel: {
+      action: "getValueLabel",
+      description:
+        "A function to get the accessible label text representing the current value in a human-readable format. If not provided, the value label will be read as the numeric value as a percentage of the max value.",
+      table: {
+        category: "Events",
+      },
+    },
+    label: {
+      control: "text",
+      description: "Optional sr-only label.",
+    },
+    max: {
+      control: { type: "number", min: 1 },
+      description: "The maximum value of the progress bar.",
+    },
+    value: {
+      control: { type: "number", min: 0, max: 100, step: 1 },
+      description: "The controlled progress value (0 to 100).",
+    },
   },
 } satisfies Meta<typeof Progress>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {
-  args: {
-    label: faker.lorem.word(),
-    max: 100,
-    value: 0,
-  },
-};
+export const Basic: Story = {};
--- src/lib/Progress/styles.ts
+++ src/lib/Progress/styles.ts
@@ -3,10 +3,10 @@ import { oneLine } from "common-tags";
 export const IndicatorStyles = oneLine`
 bg-primary-500
 duration-660
-ease-[cubic-bezier(0.65,0,0.35,1)]
+ease-linear
 h-full
 progress-indicator
-transition-all
+transition-[width]
 w-full
 
 data-[state=complete]:bg-primary-400
